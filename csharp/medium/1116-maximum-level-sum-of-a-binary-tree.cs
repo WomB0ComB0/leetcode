b0678c30af9774d1332d5bbe3ee24d7e0ba0ee2c
Mike Odnis <mike@mikeodnis.dev>
2026-01-06 23:55:37 +0000

    chore: automated daily challenge update [skip ci]

---
+++ csharp/medium/1116-maximum-level-sum-of-a-binary-tree.cs
@@ -0,0 +1,51 @@
+/*
+Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+
+ 
+Example 1:
+
+
+Input: root = [1,7,0,7,-8,null,null]
+Output: 2
+Explanation: 
+Level 1 sum = 1.
+Level 2 sum = 7 + 0 = 7.
+Level 3 sum = 7 + -8 = -1.
+So we return the level with the maximum sum which is level 2.
+
+
+Example 2:
+
+
+Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
+Output: 2
+
+
+ 
+Constraints:
+
+
+	The number of nodes in the tree is in the range [1, 104].
+	-105 <= Node.val <= 105
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MaxLevelSum(TreeNode root) {
+        
+    }
+}
