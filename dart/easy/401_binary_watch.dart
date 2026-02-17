440bcba584b91094a4e66a07f5d70e2a3ec0c584
Mike Odnis <mike@mikeodnis.dev>
2026-02-17 00:02:43 +0000

    chore: automated daily challenge update [skip ci]

--- dart/easy/401_binary_watch.dart
+++ dart/easy/401_binary_watch.dart
@@ -36,9 +36,8 @@ Constraints:
 	0 <= turnedOn <= 10
 */
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-char** readBinaryWatch(int turnedOn, int* returnSize) {
+class Solution {
+  List<String> readBinaryWatch(int turnedOn) {
     
+  }
 }
